1e8bf6311bbf8f68b306a3198cae868942d3de50
goncalogomes <gonndr@gmail.com>
2025-03-04 00:38:54 +0000

    organize imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
+import './App.scss';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
-import './App.scss';
 
 function App() {
   const [count, setCount] = useState(0);
