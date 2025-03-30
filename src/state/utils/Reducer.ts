b937a1b3f0dafd1631be7bd41c85ce6e9d7e89dd
goncalogomes <gonndr@gmail.com>
2025-03-30 11:31:48 +0100

    add addApiCase to Reducer class

--- src/state/utils/Reducer.ts
+++ src/state/utils/Reducer.ts
@@ -1,4 +1,9 @@
-import { ActionCreator, CaseReducer, Reduce } from './types';
+import { ActionCreator, ApiActions, CaseReducer, Reduce } from './types';
+
+type ApiCaseReducers<State, Success, Fail> = {
+  onSuccess?: CaseReducer<State, Success>;
+  onError?: CaseReducer<State, Fail>;
+};
 
 class Reducer<S> {
   private cases = new Map<string, CaseReducer<S>>();
@@ -7,6 +12,19 @@ class Reducer<S> {
 
   addCase<P>({ type }: ActionCreator<P>, reducer: CaseReducer<S, P>) {
     this.cases.set(type, reducer);
+    return this;
+  }
+
+  addApiCase<Success, Fail>(
+    { success, failed }: ApiActions<Success, Fail>,
+    { onSuccess, onError }: ApiCaseReducers<S, Success, Fail>
+  ) {
+    if (onSuccess) {
+      this.addCase(success, onSuccess);
+    }
+    if (onError) {
+      this.addCase(failed, onError);
+    }
     return this;
   }
 
